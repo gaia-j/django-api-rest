f01e7f109f93cfd99fbe819a127b4b74edcbc979
Hugo Gaia <hugogaia65@gmail.com>
2025-01-28 01:42:18 -0300

    fix cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -82,10 +82,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_HEADERS = (
